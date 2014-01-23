2abd0b9cc11b92a8fb4a4d0bad7b4f903218f3de
Abraham Kuri <kurenn@icalialabs.com>
2014-01-22 19:17:21 -0600

    Adds tasks for watching and compiling assets files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,24 @@
 module.exports = function(grunt) {
 
+  grunt.util.linefeed = '\n';
+
     // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    banner:'/*!\n' +
+              ' * Furatto v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+              ' * Copyright 2014-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+              ' * Licensed under <%= _.pluck(pkg.licenses, "type") %> (<%= _.pluck(pkg.licenses, "url") %>)\n' +
+              ' */\n',
 
     clean: {
       dist: 'dist'
     },
 
-    csslint: {
-      src: [
-        'dist/css/furatto.css',
-        'dist/css/examples.css',
-        'dist/css/docs.css'
-      ]
-    },
-
     concat: {
       options: {
-        separator: ';' 
+        banner: '<%= banner %>',
+        stripBanners: false
       },
       furatto: {
         src: [
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
     uglify: {
       furatto: {
         options: {
+          banner: '<%= banner %>',
           report: 'min'
         },
         src: '<%= concat.furatto.dest %>',
@@ -73,6 +74,53 @@ module.exports = function(grunt) {
     watch: {
       sass: {
         files: 'scss/furatto/*.scss',
+        tasks: 'compass:dist'
+      },
+      coffee: {
+        files: 'coffee/*.coffee',
+        tasks: ['coffee', 'concat', 'uglify']
+      }
+    },
+
+    usebanner: {
+      dist: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+            'dist/css/<%= pkg.name %>.css',
+            'dist/js/<%= pkg.name %>.js'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      docs: {
+        expand: true,
+        src: [
+          'dist/css/*',
+          'dist/js/*'
+        ],
+        dest: 'docs'
+          
+      } 
+    },
+
+    coffee: {
+      compile: {
+        options: {
+          bare: true
+        },
+        files: {
+          'js/modal.js': 'coffee/modal.coffee',
+          'js/navigation-bar.js': 'coffee/navigation-bar.coffee',
+          'js/responsiveTables.js': 'coffee/responsiveTables.coffee',
+          'js/suraido.js': 'coffee/suraido.coffee',
+          'js/toolbar.js': 'coffee/toolbar.coffee'
+        }
       }
     }
 
@@ -81,12 +129,14 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
   // CSS distribution task.
   grunt.registerTask('dist-css', ['compass:dist', 'cssmin']);
+  grunt.registerTask('dist-js', ['coffee', 'concat', 'uglify']);
+  grunt.registerTask('dist-docs', ['copy:docs']);
 
   // Docs distribution task.
   //grunt.registerTask('dist-docs', 'copy:docs');
 
   // Full distribution task.
-  grunt.registerTask('dist', ['clean', 'dist-css']);
+  grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js', 'dist-docs', 'usebanner']);
 
   // Default task.
   //grunt.registerTask('default', ['test', 'dist', 'build-glyphicons-data', 'build-customizer']);
