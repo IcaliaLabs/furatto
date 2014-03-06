edb2ebdba31b47fdd54e009b005ee722e1cc6284
Abraham Kuri <kurenn@icalialabs.com>
2014-03-06 13:34:18 -0600

    Updates the version for the compiled assets

--- dist/js/furatto.js
+++ dist/js/furatto.js
@@ -1,5 +1,11 @@
 /*!
- * Furatto v3.0.0 (http://icalialabs.github.io/furatto/)
+ * Furatto v3.0.1 (http://icalialabs.github.io/furatto/)
+ * Copyright 2014-2014 Icalia Labs
+ * Licensed under MIT (https://github.com/IcaliaLabs/furatto/blob/master/LICENSE)
+ */
+
+/*!
+ * Furatto v3.0.1 (http://icalialabs.github.io/furatto/)
  * Copyright 2014-2014 Icalia Labs
  * Licensed under MIT (https://github.com/IcaliaLabs/furatto/blob/master/LICENSE)
  */
