610c2f1decc49267288915b22a734dcd62cbed3e
Abraham Kuri <kurenn@icalialabs.com>
2014-03-20 13:52:06 -0600

    Updates the version on bower, package and config files

--- dist/js/furatto.js
+++ dist/js/furatto.js
@@ -1559,17 +1559,22 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
 }(window.jQuery);
 
 jQuery(function() {
-  $('.with-dropdown').on('touchstart click', function(e) {
-    e.preventDefault();
-    return $(this).toggleClass('opened');
+  $(document).mouseup(function(e) {
+    var dropdown_container;
+    dropdown_container = $('.with-dropdown');
+    if (!dropdown_container.is(e.target) && dropdown_container.has(e.target).length === 0) {
+      return dropdown_container.removeClass('opened');
+    }
   });
-  return $('.with-dropdown').mouseenter(function(e) {
-    $('.with-dropdown .dropdown li a').click(function(e) {
-      return e.stopPropagation();
-    });
-    return $(this).addClass('opened');
-  }).mouseleave(function() {
-    return $(this).removeClass('opened');
+  $(document).on('touchstart click', '.with-dropdown', function(e) {
+    var $container;
+    e.preventDefault();
+    $container = $(e.target).parent();
+    $container.siblings('.with-dropdown').removeClass('opened');
+    return $container.toggleClass('opened');
+  });
+  return $(document).on('touchstart click', '.with-dropdown > ul.dropdown', function(e) {
+    return e.stopPropagation();
   });
 });
 
